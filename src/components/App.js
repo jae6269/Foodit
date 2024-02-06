6971de770079f4fe746378e4acb6c8c0fd57b611
jae6269 <jae6269@gmail.com>
2024-02-07 00:35:59 +0900

    feat : 버튼 클릭으로 최신순, 칼로리순 정렬기능구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import FoodList from './FoodList';
 import items from '../mock.json';
+import { useState } from 'react';
 
 function App() {
+  //정렬기준 state
+  const [order, setOrder] = useState('createdAt');
+
+  //정렬기준 변경하는 함수
+  const handleNewestClick = () => setOrder('createdAt');
+  const handleCalorieClick = () => setOrder('calorie');
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
   return (
-    <div>
-      <FoodList items={items} />
-    </div>
+    <>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleCalorieClick}>칼로리순</button>
+      </div>
+      <div>
+        <FoodList items={sortedItems} />
+      </div>
+    </>
   );
 }
 
